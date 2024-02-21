cd0bceac66b16373610ac7c62972c583fa69f150
RichardRodi <RichardmRodi@gmail.com>
2024-02-21 14:36:08 -0500

    Added all 15 lines of the for loop.

---
+++ LoopsExercise-1/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace LoopsExercise_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         //Exercise 1
+         for(int i = 0; i< 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+         for(int i= 0; i<5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+          for(int i= 0;i< 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+          for(int i= 0; i<5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i<5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i<5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i<5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i<5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i<5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i<5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i<5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i<5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i<5; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+            for (int i = 0; i < 5: i++)
+                Console.WriteLine($"{i}");
+        }
+    }
+}
